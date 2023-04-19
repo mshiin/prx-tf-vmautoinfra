37a66dc96c7ae580d49814bed641b60e749adffd
mshin <122724022+mshiin@users.noreply.github.com>
2023-04-19 15:54:50 +1000

    Update main.tf comments

--- vm-creation-proxmox/main.tf
+++ vm-creation-proxmox/main.tf
@@ -1,5 +1,6 @@
 # This is where you deploy resources
-# Couldn't create module for proxmox_vm_qeumu as proxmox provider has not released any modules yet 
+# Couldn't create module for proxmox_vm_qeumu as proxmox provider has not released any modules yet
+# Good practice to separate modules in another directory and reference them in main.tf
 # 'proxmox_vm_qemu' resource type and resource_name is resource name
 # proxmox_vm_qumu
 resource "proxmox_vm_qemu" "resource_name" {
